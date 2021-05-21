0fc3da2078a42adf4e69cc1266d3f11d5a9cf901
Himu1503 <54092681+Himu1503@users.noreply.github.com>
2021-05-21 17:33:52 +0530

    User Schema done

---
+++ projbackend/modules/user.js
@@ -0,0 +1,94 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+import { createHmac } from 'crypto'
+const { v4: uuidv4 } = require('uuid')
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        trim:true,
+        maxlength:32,
+        required:true
+    },
+
+    lastname:{
+        type:String,
+        maxlength:32,
+        trim:true
+
+    },
+
+    email:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true
+
+
+    },
+
+    userinfo:{
+        type:String,
+        trim:true
+
+    },
+
+    encry_password:{
+        type:String,
+        required:true
+
+    },
+
+salt:String,
+
+role:{
+    type:Number,
+    default:0
+},
+
+purchases:{
+    type: Array,
+    default: []
+}
+
+});
+
+userSchema.virtual("password")
+
+   .set(function(password) {
+       this._password=password
+       this.salt= uuidv4()
+       this.encry_password=secureFunction(password)
+   })
+   .get(function() {
+       return this.password
+   })
+
+
+userSchema.method ={
+
+
+
+    authenticate: function (password) {
+        return this.secureFunction(plainpassword)=== this.encry_password
+
+    },
+    
+    secureFunction: function (plainpassword) {
+     if (!password) return ""
+     try {
+
+          return createHmac('sha256', this.salt)
+               .update(plainpassword)
+               .digest('hex')
+         
+     } catch (err) {
+         return ""
+         
+     }
+
+
+    }
+}
+
+module.exports = mongoose.model("User",userSchema)
