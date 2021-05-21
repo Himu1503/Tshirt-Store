0e34ec934c525f1a7c0ecebb34719e650f06b778
Himu1503 <54092681+Himu1503@users.noreply.github.com>
2021-05-21 18:21:54 +0530

    order schema

---
+++ projbackend/modules/order.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+const {ObjectId} = mongoose.Schema
+
+
+const ProductCartSchema= new Schema ({
+    product:{
+        type: ObjectId,
+        ref:"Product"
+    },
+
+    name:String,
+    count:Number,
+    price:Number
+
+}) 
+
+const OrderSchema = new Schema({
+   products: [ProductCartSchema],
+   transaction_id:{},
+   amount:{type:Number},
+   address:String, 
+   updated: Date,
+   user: {
+       type: ObjectId,
+       ref:"User"
+   }
+
+}, {timestamps:true});
+
+const Order = mongoose.model ("Order",OrderSchema)
