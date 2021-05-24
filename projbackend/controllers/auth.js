5f3cd7f2174c1bd8b20ccda20d5edb502c084f88
Himu1503 <54092681+Himu1503@users.noreply.github.com>
2021-05-24 16:59:49 +0530

    Express validator
    Express validator on the routes and error message to controller

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,8 +1,22 @@
 const User = require ("../modules/user")
+const { check, validationResult } = require('express-validator')
 exports.signup= (req,res)=> {
+
+const errors = validationResult(req)
+
+if (!errors.isEmpty()){
+   return res.status(422).json({
+       error:errors.array()[0].msg
+
+   })
+
+}
 
  const user = new User (req.body)
 user.save((err,user)=>{
+
+
+
 
     if (err) {
         return res.status(404).json({
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,21 @@
 var express = require('express')
 var router = express.Router()
+const { check } = require('express-validator')
 const {signout,signup} = require("../controllers/auth")
 
+//Singup route with check method express validator 
 
-router.post ("/signup" ,signup )
+router.post ("/signup" ,[
+
+    check ("name").isLength({min: 3}).withMessage('Name must be at least 3 chars long'),
+    check ("email").isEmail(),
+    check("passowrd").isLength({min: 3}).withMessage('Password must be at least 5 chars long')
+
+],signup )
+
+
+
+
 router.get("/signout",signout)
 
 
